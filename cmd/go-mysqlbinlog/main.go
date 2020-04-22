01f3d8a6bf4f3b8499fe2bd366eb2bce7ec8aaf5
linruichao <linruichao@baidu.com>
2020-04-22 18:25:37 +0800

    first commit

---
+++ cmd/go-mysqlbinlog/main.go
@@ -0,0 +1,97 @@
+// go-mysqlbinlog: a simple binlog tool to sync remote MySQL binlog.
+// go-mysqlbinlog supports semi-sync mode like facebook mysqlbinlog.
+// see http://yoshinorimatsunobu.blogspot.com/2014/04/semi-synchronous-replication-at-facebook.html
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pingcap/errors"
+	"github.com/ruiaylin/go-mysql/client"
+	"github.com/ruiaylin/go-mysql/mysql"
+	"github.com/ruiaylin/go-mysql/replication"
+)
+
+var host = flag.String("host", "127.0.0.1", "MySQL host")
+var port = flag.Int("port", 3306, "MySQL port")
+var user = flag.String("user", "root", "MySQL user, must have replication privilege")
+var password = flag.String("password", "", "MySQL password")
+
+var flavor = flag.String("flavor", "mysql", "Flavor: mysql or mariadb")
+
+var file = flag.String("file", "", "Binlog filename")
+var pos = flag.Int("pos", 4, "Binlog position")
+
+var semiSync = flag.Bool("semisync", false, "Support semi sync")
+var backupPath = flag.String("backup_path", "", "backup path to store binlog files")
+
+var rawMode = flag.Bool("raw", false, "Use raw mode")
+
+func main() {
+	flag.Parse()
+
+	cfg := replication.BinlogSyncerConfig{
+		ServerID: 101,
+		Flavor:   *flavor,
+
+		Host:            *host,
+		Port:            uint16(*port),
+		User:            *user,
+		Password:        *password,
+		RawModeEnabled:  *rawMode,
+		SemiSyncEnabled: *semiSync,
+		UseDecimal:      true,
+	}
+
+	b := replication.NewBinlogSyncer(cfg)
+
+	// get current binlog pos if the given param is none
+
+	c, err := client.Connect(fmt.Sprintf("%s:%d", *host, *port), *user, *password, "")
+	if err != nil {
+		fmt.Println("Create Connection error: ", err.Error())
+	}
+	rr, err := c.Execute("show master status")
+	binFile, _ := rr.GetString(0, 0)
+	binPos, _ := rr.GetInt(0, 1)
+	// Close the connection
+	if c != nil {
+		c.Close()
+		c = nil
+	}
+
+	pos := mysql.Position{Name: binFile, Pos: uint32(binPos)}
+	if len(*backupPath) > 0 {
+		// Backup will always use RawMode.
+		err := b.StartBackup(*backupPath, pos, 0)
+		if err != nil {
+			fmt.Printf("Start backup error: %v\n", errors.ErrorStack(err))
+			return
+		}
+	} else {
+		s, err := b.StartSync(pos)
+		if err != nil {
+			fmt.Printf("Start sync error: %v\n", errors.ErrorStack(err))
+			return
+		}
+
+		for {
+			e, err := s.GetEvent(context.Background())
+			if err != nil {
+				// Try to output all left events
+				events := s.DumpEvents()
+				for _, e := range events {
+					e.Dump(os.Stdout)
+				}
+				fmt.Printf("Get event error: %v\n", errors.ErrorStack(err))
+				return
+			}
+
+			e.Dump(os.Stdout)
+		}
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/ruiaylin/go-mysql
+
+go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/pingcap/check v0.0.0-20190102082844-67f458068fc8
+	github.com/pingcap/errors v0.11.0
+	github.com/pingcap/parser v0.0.0-20190506092653-e336082eb825
+	github.com/pingcap/tipb v0.0.0-20190428032612-535e1abaa330 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726
+	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07
+	github.com/siddontang/go-mysql v0.0.0-20191114035249-07ca7840b812 // indirect
+	github.com/sirupsen/logrus v1.4.1 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
